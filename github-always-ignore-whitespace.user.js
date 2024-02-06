b6a2ed2cc5d26a22456e67c3597f268e072003ee
Yu Matsuzawa <yu.matsuzawa@siiibo.com>
2024-02-06 18:55:35 +0900

    fix: use new URL

--- github-always-ignore-whitespace.user.js
+++ github-always-ignore-whitespace.user.js
@@ -2,7 +2,7 @@
 // @name         Always ignore whitespace in GitHub Pull Request diffs
 // @namespace    https://ymtszw.cc
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
-// @version      1.20240206.1
+// @version      1.20240206.2
 // @description  Always ignore whitespace in GitHub Pull Request diffs
 // @author       Gada / ymtszw
 // @copyright    2023, Gada / ymtszw (https://ymtszw.cc)
@@ -19,11 +19,10 @@
   "use strict";
   const q = new URLSearchParams(window.location.search);
   if (!q.has("w")) {
-    q.set("w", "1");
-    window.location.search = q.toString();
-    const newUrl = window.location.href;
-    console.log(`Redirecting to ${newUrl}`);
-    window.location.assign(newUrl);
+    console.log("Ignoring whitespace...");
+    const newUrl = new URL(window.location);
+    newUrl.searchParams.set("w", "1");
+    window.location.assign(newUrl.toString());
   } else {
     console.log("Already ignoring whitespace");
   }
