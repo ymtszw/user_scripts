bc8878c446cf4935dd044d9c5c2b6019098f521c
Yu Matsuzawa <yu.matsuzawa@siiibo.com>
2024-02-06 19:35:38 +0900

    feat: more global handler

--- github-always-ignore-whitespace.user.js
+++ github-always-ignore-whitespace.user.js
@@ -2,7 +2,7 @@
 // @name         Always ignore whitespace in GitHub Pull Request diffs
 // @namespace    https://ymtszw.cc
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
-// @version      1.20240206.6
+// @version      1.20240206.7
 // @description  Always ignore whitespace in GitHub Pull Request diffs
 // @author       Gada / ymtszw
 // @copyright    2023, Gada / ymtszw (https://ymtszw.cc)
@@ -31,19 +31,19 @@ const filesViewRegex = new RegExp("https://github.com/.+/.+/pull/.+/files");
 
 (function () {
   "use strict";
+  // GitHubは要所要所でTurbolinksを使っていて、リンククリック時の挙動がclient-side routingでもなければ通常のページ遷移でもないことがある
+  // Files viewへのリンクはその一つなので、popstateやhashchangeイベントで検知できない
+  // そこでclickイベントを直接監視し、files viewへのリンククリックだったらリダイレクトするようなハンドラを大域追加する
+  console.log("Inject click event handler for turbolinks.");
+  window.addEventListener("click", (e) => {
+    if (e.target?.href?.match(filesViewRegex)) {
+      console.log("Navigating to files view. Intercepting...");
+      checkUrlAndRedirect(new URL(e.target.href));
+    }
+  });
+
   if (window.location.href.match(filesViewRegex)) {
     // Files viewのURLを直接開いた場合には即座にオプションをチェックする
     checkUrlAndRedirect();
-  } else {
-    // GitHubは要所要所でTurbolinksを使っていて、リンククリック時の挙動がclient-side routingでもなければ通常のページ遷移でもないことがある
-    // Files viewへのリンクはその一つなので、popstateやhashchangeイベントで検知できない
-    // そこでclickイベントを直接監視し、files viewへのリンククリックだったらリダイレクトするようなハンドラを大域追加する
-    console.log("Inject click event handler for turbolinks.");
-    window.addEventListener("click", (e) => {
-      if (e.target?.href?.match(filesViewRegex)) {
-        console.log("Navigating to files view. Intercepting...");
-        checkUrlAndRedirect(new URL(e.target.href));
-      }
-    });
   }
 })();
