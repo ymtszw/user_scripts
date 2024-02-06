c74d620f03c88a6197da252cad3f3d561e97634c
Yu Matsuzawa <yu.matsuzawa@siiibo.com>
2024-02-06 19:01:33 +0900

    feat: check if client-side routed

--- github-always-ignore-whitespace.user.js
+++ github-always-ignore-whitespace.user.js
@@ -2,7 +2,7 @@
 // @name         Always ignore whitespace in GitHub Pull Request diffs
 // @namespace    https://ymtszw.cc
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
-// @version      1.20240206.2
+// @version      1.20240206.3
 // @description  Always ignore whitespace in GitHub Pull Request diffs
 // @author       Gada / ymtszw
 // @copyright    2023, Gada / ymtszw (https://ymtszw.cc)
@@ -10,14 +10,14 @@
 // @updateURL    https://raw.githubusercontent.com/ymtszw/user_scripts/main/github-always-ignore-whitespace.user.js
 
 // @noframes     true
-// @run-at       document-idle
+// @run-at       document-start
 // @match        https://github.com/*/*/pull/*/files*
 // @grant        none
 // ==/UserScript==
 
-(function () {
-  "use strict";
+function checkUrlAndRedirect() {
   const q = new URLSearchParams(window.location.search);
+
   if (!q.has("w")) {
     console.log("Ignoring whitespace...");
     const newUrl = new URL(window.location);
@@ -26,4 +26,12 @@
   } else {
     console.log("Already ignoring whitespace");
   }
+}
+
+(function () {
+  "use strict";
+  checkUrlAndRedirect();
+  window.addEventListener("popstate", (e) => {
+    console.log("popstate", e);
+  });
 })();
