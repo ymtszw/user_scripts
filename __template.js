733efaddf375e9fb1d657ce2122d34a556ce1740
Yu Matsuzawa <yu.matsuzawa@siiibo.com>
2024-02-06 18:39:25 +0900

    chore: add __template

---
+++ __template.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         TODO fill
+// @namespace    https://ymtszw.cc
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=TODO
+// @version      1.[TODO 20YYMMDD].0
+// @description  TODO fill
+// @author       Gada / ymtszw
+// @copyright    2023, Gada / ymtszw (https://ymtszw.cc)
+// @downloadURL  https://raw.githubusercontent.com/ymtszw/user_scripts/main/TODO.user.js
+// @updateURL    https://raw.githubusercontent.com/ymtszw/user_scripts/main/TODO.user.js
+
+// @noframes     true
+// @run-at       document-idle
+// @match        https://TODO
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  console.log("TODO");
+})();
