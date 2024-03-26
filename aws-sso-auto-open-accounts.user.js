c59bdb58166e4ce4d4da12a59ac0e390b368735d
Yu Matsuzawa <yu.matsuzawa@siiibo.com>
2024-03-26 16:15:54 +0900

    feat: update aws-sso-auto-open

--- aws-sso-auto-open-accounts.user.js
+++ aws-sso-auto-open-accounts.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Auto-open AWS SSO Accounts in start page
+// @name         Auto-open AWS SSO Account
 // @namespace    https://ymtszw.cc
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=awsapps.com
-// @version      1.20240206.0
-// @description  Auto-open AWS SSO Accounts in start page
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=amazonaws.com
+// @version      1.20240326.2
+// @description  Auto-open AWS SSO Account
 // @author       Gada / ymtszw
 // @copyright    2023, Gada / ymtszw (https://ymtszw.cc)
 // @downloadURL  https://raw.githubusercontent.com/ymtszw/user_scripts/main/aws-sso-auto-open-accounts.user.js
@@ -19,12 +19,12 @@
 function findAndClickAccounts(remaining_attempt) {
   return function (timestamp) {
     if (remaining_attempt > 0) {
-      if (document.querySelector("portal-instance-list")) {
+      if (document.querySelector('[data-testid="account-list"]')) {
         for (const kid of document.querySelectorAll(
-          "portal-instance-list .instance-section"
+          '[data-testid="account-list"] button[data-testid="account-list-cell"]'
         )) {
           kid.click();
-          const name = kid.textContent.trim().split(/\s+/)[0];
+          const name = kid.querySelector("strong").textContent;
           console.log(`Account (${name}) found and clicked!`);
         }
       } else {
@@ -38,25 +38,7 @@ function findAndClickAccounts(remaining_attempt) {
   };
 }
 
-function findAndClickMenu(remaining_attempt) {
-  return function (timestamp) {
-    if (remaining_attempt > 0) {
-      if (document.querySelector('[title="AWS Account"]')) {
-        document.querySelector('[title="AWS Account"]').click();
-        console.log(`Menu found and clicked!`);
-        window.requestAnimationFrame(
-          findAndClickAccounts(remaining_attempt - 1)
-        );
-      } else {
-        window.requestAnimationFrame(findAndClickMenu(remaining_attempt - 1));
-      }
-    } else {
-      console.log(`Menu not found`);
-    }
-  };
-}
-
 (function () {
   "use strict";
-  window.requestAnimationFrame(findAndClickMenu(1_000));
+  window.requestAnimationFrame(findAndClickAccounts(1_000));
 })();
